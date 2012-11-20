c40cc1b1c8022b77b9dafe8eb91db3ff3673a3cb
Filipe Coelho dos Santos Manoel <filipemanoel@gmail.com>
2012-11-20 12:39:15 -0200

    Alteração na página de servicços
    Flata ajustar algumas coisas

--- servicos/index.php
+++ servicos/index.php
@@ -17,24 +17,37 @@ include("../includes/menu.php");
 <div class="servicos_principal">
  <h1>Serviços - O que fazemos</h1>
  <p class="destaque">SEO, Consultoria Google AdWords, Facebook Marketing, Landing Pages, otimização de sites, consultoria Google Analytics, integrados  para que sua empresa aumente as vendas pela Internet.</p>
+
  <p>A SEO Marketing Brasil é uma agência 100% especializada em aumentar suas vendas pela internet.  Oferecemos pacotes de serviços de SEO, links patrocinados AdWords e Landing Pages para tornar o Google sua ferramenta de negócios mais lucrativa e eficiente. Criamos a sua presença no Facebook e demais redes sociais com a cara da sua empresa, engajando clientes e potenciais de forma a construir um relacionamento eficiente e de longo prazo. Medimos os resultados de todas as ações através do Google Analytics de uma forma que você consiga facilmente entender a evolução da campanha.</p>
  <div class="servicos_header">
   <div class="menu_servico">
-   <div class="adw_icone">&nbsp;</div>
-   <a class="adw_saiba_mais" href="<?php echo $dominio;?>links-patrocinados/"></a></div>
+   <div class="servico_content"><h2 class="adw_link"><a class="adw_link" href="<?php echo $dominio;?>links-patrocinados/">Google AdWords <br/>Links Patrocinados</a></h2>
+      <h3>Criação, administração, consultoria de anúncios no Google, Banners, Landing Pages</h3>
+      <p>Com dezenas ou centenas de concorrentes competindo pelo mesmo espaço nos resultados  do Google, contar com uma agência com 6 anos de experiência, certificada pelo Google e com centenas de clientes atendidos podem significar a diferença entre o sucesso e o fracasso de suas campanhas no Google.</p></div>
+   </div>
   <div class="menu_servico">
-   <div class="seo_icone">&nbsp;</div>
-   <a class="seo_saiba_mais" href="<?php echo $dominio;?>seo/"></a></div>
+   <div class="servico_content">
+   <h2 class="seo_link"><a class="seo_link" href="<?php echo $dominio;?>seo/">SEO Google <br />Otimização de Sites</a></h2>
+      <h3>Consultoria, Avaliação, Implementação on-page, Link Building</h3>  
+       <p>Nascemos fazendo SEO, SEO é nosso nome. Conte com a experiência de nossos consultores e analistas para aumentar significativamente o tráfego de visitas vindos dos resultados gratuitos do Google. </p></div>
+   </div>
   <div class="menu_servico">
-   <div class="soc_icone">&nbsp;</div>
-   <a class="soc_saiba_mais" href="<?php echo $dominio;?>social-media/"></a></div>
+  <div class="servico_content">
+ <h2 class="soc_link"><a class="soc_link" href="<?php echo $dominio;?>social-media/">Redes Sociais</a></h2>
+      <h3>Planejamento,  Fanpages, Faebook Ads, aumento de base de seguidores, engajamento, monitoramento e SAC 2.0</h3>
+       <p>Com um time de jornalistas e redatores especializados em criar um relacionamento duradouro e rentável de empresas com sua base de clientes existentes e potenciais, a SEO Marketing alia a estratégia de Redes Sociais a iniciativas de SEO e anúncios na internet, monitorando em detalhes o desempenho de sua presença nas redes.</p></div>
+   </div>
   <div class="menu_servico">
-   <div class="ana_icone">&nbsp;</div>
-   <a class="ana_saiba_mais" href="<?php echo $dominio;?>web-analytics/"></a></div>
+  <div class="servico_content">
+  <h2 class="ana_link"><a class="ana_link" href="<?php echo $dominio;?>web-analytics/">Google Analytics <br />Deep Analysis</a></h2>
+  <h3>Consultoria, Suporte, Instalação, Customização, Relatórios e-commerce</h3>
+  <p>Saber cada passo dos visitantes de seu site é o primeiro passo para definir as alterações necesárias e multiplicar (mesmo) as vendas de de seu site. Seja o seu conhecimento em Analytics básico ou avançado, nossa experiência em mais de 400 contas Analytics lhe dará preciosas dicas para aumentar as vendas de seu site. </p>
+  </div>
+   </div>
  </div>
  <br/>
  <div class="servicos_tbl">
-  <div>
+  <!--<div>
    <div class="item">
     <div>
      <div class="title">
@@ -80,7 +93,7 @@ include("../includes/menu.php");
     <p>Saber cada passo dos visitantes de seu site é o primeiro passo para definir as alterações necesárias e multiplicar (mesmo) as vendas de de seu site. Seja o seu conhecimento em Analytics básico ou avançado, nossa experiência em mais de 400 contas Analytics lhe dará preciosas dicas para aumentar as vendas de seu site. </p>
    </div>
   </div>
- </div>
+ </div>-->
  <div>
   <p>Seja qual for o perfil da sua empresa, a SEO Marketing pode auxiliá-la a destacar-se no Google e Redes Sociais para diferenciar-se de seus concorrentes. Contrate nossos serviços individualmente ou em um pacote que cubra as suas necessidades de exposição e conversão de vendas.</p>
  </div>
